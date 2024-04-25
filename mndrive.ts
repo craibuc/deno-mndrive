9d49ccc4686636ada688ed7c25515e6a29644867
Craig Buchanan <craig.buchanan@cogniza.com>
2024-04-25 01:11:14 +0000

    adding generate_password and set_password

--- mndrive.ts
+++ mndrive.ts
@@ -1,7 +1,7 @@
 export class MnDrive {
 
   private authorization: string;
-  private base_uri: string;
+  public base_uri: string;
 
   constructor (
     server: 'Testing'|'Staging'|'Production', 
@@ -40,7 +40,7 @@ export class MnDrive {
   get_account_status = async () => {
     
       const url = `${this.base_uri}/Services/Maintenance/Rest/ExpirationCheck`;
-      console.log("url", url);
+      // console.log("url", url);
       
       return await fetch(url, {
         method: "GET",
@@ -64,7 +64,7 @@ export class MnDrive {
   ) => {
     
     const url = `${this.base_uri}/Services/RecordRequest/v1/Search`;
-    console.log("url", url);
+    // console.log("url", url);
     
     return await fetch(url, {
         method: "POST",
@@ -86,5 +86,88 @@ export class MnDrive {
     .then((data)=> data);
 
   }
+
+  set_password = async (
+    newpassword: string
+  ) => {
   
+    const url = `${this.base_uri}/Services/Maintenance/Rest/ChangePassword`;
+    // console.log("url", url);
+    
+    return await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: this.authorization,
+        'Content-Type': "application/json",
+      },
+      body: JSON.stringify({
+        "newpassword": `${newpassword}`,
+      }),
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.statusText} [${response.status}]`);
+      }
+      return response.json();
+    })
+    .then((data)=> data);
+
+  }
+
+  generate_password = () => {
+    /**
+     * @param {number} length Length of the password.
+     * @param {string} wishlist List of characters to be used in the password.
+     * @returns {string} The password
+     */
+    const generatePassword = (
+        length: number = 20,
+        wishlist: string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@$%*()?:;{}+-~",
+    ) =>
+      /*
+      array of 20 random numbers
+      x % wishlist.length: produces a value between 0 and length-1
+      wishlist[x % wishlist.length]): gets the character with that index
+      */
+      Array.from(crypto.getRandomValues(new Uint32Array(length)))
+        .map((x) => wishlist[x % wishlist.length])
+        .join("");
+  
+    /*
+    ^ - beginning of line
+    (?!.*[&<>]) - none of these characters
+    (?=.*\d) - one number
+    (?=.*[a-z]) - one, lower-case letter
+    (?=.*[A-Z]) - one, upper-case letter
+    (?=.*[!@$%*()?:;{}\+\-~]) - one, special character
+    .{14,28} - match 14 to 28 times
+    $ - end of line
+    */
+    const re = /^(?!.*[&<>])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@$%*()?:;{}\+\-~]).{14,28}$/g;
+  
+    let matched = false
+    let password = null
+  
+    // min and max included 
+    const randomIntFromInterval = (min: number, max: number): number  => Math.floor(Math.random() * (max - min + 1) + min);
+    
+    do {
+  
+      password = generatePassword( randomIntFromInterval(14,28) )
+      console.log('password',password)
+  
+      matched = re.test(password);
+      console.log('matched',matched)
+  
+      if (matched) { break; }
+  
+    } while ( matched === false );
+  
+    return {
+        Password: password
+    };
+  
+  }
+
 }
